e84aa18d725c7dee1b23e66556c0479399314e3d
Hiky <hikygm@ya.ru>
2022-11-14 11:01:13 +0500

    comlete register

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, request, redirect
 import sqlite3
 from static.database import db_session
@@ -116,7 +118,8 @@ def gallery():
 
 def main():
     db_session.global_init("static/database/database.db")
-    app.run(port=8080, host='127.0.0.1')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 def categories():
